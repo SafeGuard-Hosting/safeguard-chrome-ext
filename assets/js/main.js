25ee5cb177b829c2cd700e635231c42bee72cbda
Ethan Figliola <ethanfiggs@msn.com>
2023-08-18 14:48:02 -0400

    Massive UI Upgrade utilizing Bootsrap & templates

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+// Output a message to the console
+console.log('This is a popup!');
+
+// Function to load a template and inject it into a specified element
+function loadTemplate(url, elementId, callback) {
+  fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(elementId).innerHTML = data;
+      if (typeof callback === 'function') {
+        callback();
+      }
+    })
+    .catch(error => {
+      console.error('Error loading template:', error);
+    });
+}
+
+// Load header template
+loadTemplate('/include/template/nav.html', 'nav-placeholder');
+
+// Load footer template
+loadTemplate('/include/template/footer.html', 'footer-placeholder');
+
+// Load footer template and set current year after loading
+loadTemplate('/include/template/footer.html', 'footer-placeholder', function() {
+  // Get the current year
+  const currentYear = new Date().getFullYear();
+
+  // Set the current year in the span element
+  const currentYearSpan = document.getElementById("currentYear");
+  if (currentYearSpan) {
+    currentYearSpan.textContent = currentYear;
+  }
+});
